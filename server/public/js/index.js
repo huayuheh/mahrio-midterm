70f176e7e6749167561d236e56fe59719f3acf17
Jesus Guerrero <rocha.jesus@hotmail.com>
2016-10-11 05:25:29 +0000

    Setup angular, add css, and reconfigure socket event to two-way data bind

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -4,6 +4,13 @@ socket.on('event:hello', function(){
   console.log('Hello from server through socket');
 });
 
-socket.on('event:motion', function( val ) {
-  console.log('Motion in 21 is ' +(val ? 'ACTIVE' : 'INACTIVE'));
-});
+angular.module('mahrio-motion-client', [])
+  .controller('MainCtrl', ['$scope', function( $scope ) {
+    $scope.motion = false;
+    socket.on('event:motion', function( val ) {
+      console.log('Motion in 21 is ' +(val ? 'ACTIVE' : 'INACTIVE'));
+      $scope.$apply( function() {
+        $scope.motion = val ? true : false;
+      });
+    });
+  }]);
