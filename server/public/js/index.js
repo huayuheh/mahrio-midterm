bf79b422e166738853d88507c383131ca8a700bb
Jesus Guerrero <rocha.jesus@hotmail.com>
2016-10-11 05:09:07 +0000

    Socket listening in client for motion event

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('event:hello', function(){
   console.log('Hello from server through socket');
 });
+
+socket.on('event:motion', function( val ) {
+  console.log('Motion in 21 is ' +(val ? 'ACTIVE' : 'INACTIVE'));
+});
